e3c456c27de549c169e677728b801d15f1e62110
Adan Moreno <81122473+Hachikoi-the-creator@users.noreply.github.com>
2023-12-11 09:25:40 -0600

    testin wa link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function page() {
-    return <div>page</div>;
+    return (
+        <div>
+            <h1>Testing wats link</h1>
+            <a
+                href={`https://wa.me/${5215592252223}/?text=${encodeURIComponent(
+                    "Hello there! cutie pie"
+                )}`}
+            >
+                13 nums link
+            </a>
+            <a
+                href={`https://wa.me/${5592252223}/?text=${encodeURIComponent(
+                    "Hello there! cutie pie x2"
+                )}`}
+            >
+                10 nums link
+            </a>
+        </div>
+    );
 }
