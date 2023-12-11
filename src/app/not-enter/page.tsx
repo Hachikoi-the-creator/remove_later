4e102e372d51282927b11dcae961032b4caf55ae
Adan Moreno <adan.moreno@heyhom.mx>
2023-12-11 13:13:02 -0600

    w root/[dynaimc] cannot really get to sitemaps

---
+++ src/app/not-enter/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+    return <div>DONT ENTER HERE GOOGLE ROBOTS</div>;
+}
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,6 +5,7 @@ export default function robots(): MetadataRoute.Robots {
         rules: {
             userAgent: "*",
             allow: "/",
+            disallow: ["/not-enter/", "/private/"],
         },
         sitemap: "https://babu-app.vercel.app.com/sitemap.xml",
     };
